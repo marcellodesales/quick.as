6e52578c8abeedaa09bac71cf12c069d3be826b8
Neil Kinnish <neilkinnish@Neils-MacBook-Pro.local>
2013-07-03 22:40:48 +0100

    switched to quickcast

--- tests/users.js
+++ tests/users.js
@@ -29,10 +29,10 @@ var should  = require('should');
     });
 });*/
 
-describe('Get /v1/users/userbytoken', function(){
+describe('Get /api/v1/users/userbytoken', function(){
     it('should respond json',function(done){
         request(app)
-        .get('/v1/users/userbytoken')
+        .get('/api/v1/users/userbytoken')
         .set('Accept', 'application/json')
         .set('token', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImhlbGxvQG5laWxraW5uaXNoLmNvbSJ9.niqSfyyJBOK_ArST0JW2x43yyM5tDNcoubnsBxHxayc')
         .expect(200)
@@ -44,10 +44,10 @@ describe('Get /v1/users/userbytoken', function(){
     });
 });
 
-describe('Get /v1/users/signin no details', function(){
+describe('Get /api/v1/users/signin no details', function(){
     it('should respond json',function(done){
         request(app)
-        .post('/v1/users/signin')
+        .post('/api/v1/users/signin')
         .expect(401)
         .expect('Content-Type', /json/)
         .end(function(err, res){
@@ -57,10 +57,10 @@ describe('Get /v1/users/signin no details', function(){
     });
 });
 
-describe('Get /v1/users/signin with details', function(){
+describe('Get /api/v1/users/signin with details', function(){
     it('should respond json',function(done){
         request(app)
-        .post('/v1/users/signin')
+        .post('/api/v1/users/signin')
         .set('username', 'neil')
         .set('password', 'neil')
         .expect(200)
@@ -72,10 +72,10 @@ describe('Get /v1/users/signin with details', function(){
     });
 });
 
-describe('Get /v1/users/signup no details', function(){
+describe('Get /api/v1/users/signup no details', function(){
     it('should respond json',function(done){
         request(app)
-        .post('/v1/users/signup')
+        .post('/api/v1/users/signup')
         .expect(403)
         .expect('Content-Type', /json/)
         .end(function(err, res){
@@ -85,10 +85,10 @@ describe('Get /v1/users/signup no details', function(){
     });
 });
 
-describe('Get /v1/users/signup username + email in use', function(){
+describe('Get /api/v1/users/signup username + email in use', function(){
     it('should respond json',function(done){
         request(app)
-        .post('/v1/users/signup')
+        .post('/api/v1/users/signup')
         .set('username', 'neil')
         .set('email','hello@neilkinnish.com')
         .expect(403)
@@ -97,6 +97,26 @@ describe('Get /v1/users/signup username + email in use', function(){
           if (err) return done(err);
           res.text.should.include('Username already in use')
           res.text.should.include('Email already in use')
+          done()
+        });
+    });
+});
+
+describe('Get /api/v1/users/signup username + email in use', function(){
+    it('should respond json',function(done){
+        request(app)
+        .post('/api/v1/users/signup')
+        .set('username', 'neil')
+        .set('email','hello@neilkinnish.com')
+        .expect(403)
+        .expect('Content-Type', /json/)
+        .end(function(err, res){
+          if (err) return done(err);
+          res.text.should.include('Username already in use')
+          res.text.should.include('Email already in use')
+          res.text.should.include('Firstname is required')
+          res.text.should.include('Lastname is required')
+          res.text.should.include('Password is required')
           done()
         });
     });
