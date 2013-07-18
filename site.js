99c98cb8f1e638b48e5c3211e58582166a7b14a3
Neil Kinnish <neilkinnish@Neils-MacBook-Pro.local>
2013-07-18 20:33:36 +0100

    updated

--- site.js
+++ site.js
@@ -33,76 +33,81 @@ exports.video = function(req, res) {
 
     client.query("SELECT casts.*, users.username, tags.* FROM casts INNER JOIN users ON (casts.ownerid = users.userid) INNER JOIN WHERE lower(casts.uniqueid) = $1 AND casts.published = true", [video_entry.toLowerCase()], function(err1, result1){
     	client.end();
-		if (!err1 && result1 != undefined && result1.rowCount > 0){
-			var data = result1.rows[0];
+
+    	if (err1) {
+    		res.render('500', 500);
+    		return;
+    	}
+
+		if (result1 === undefined && !result1.rows){
+			res.render('404', 404);
+			return;
+		}
+		
+		var data = result1.rows[0];
 
 			client.query("SELECT tags.name FROM casts_tags INNER JOIN tags ON (casts_tags.tagid = tags.tagid) WHERE casts_tags.castid = $1", [data.castid], function(err2, result2){
 				
-				client.end();
+			client.end();
 
-				var tags = null;
+			var tags = null;
 
-				if (!err2 && result2 != undefined && result2.rowCount > 0){
-					tags = result2.rows;
-				}
+			if (!err2 && result2 != undefined && result2.rows){
+				tags = result2.rows;
+			}
 
-				utilities.logViews(video_entry, req, function(err, r) {
-					marked(data.description, markedOpts, function (err, content) {
-						if (err) throw err;
+			utilities.logViews(video_entry, req, function(err, r) {
+				marked(data.description, markedOpts, function (err3, content) {
+					if (err3) {
+						content = "Error converting Markdown!";
+					}
 
-						var a = moment(data.created);
-						var b = moment(new Date());
+					var a = moment(data.created);
+					var b = moment(new Date());
 
-						var duration = moment(data.created).hours();
+					var duration = moment(data.created).hours();
 
-						var str = 'https://s3.amazonaws.com/quickcast/%s/%s/quickcast.%s';
-						var fileCheck = '/%s/%s/quickcast.%s';
+					var str = 'https://s3.amazonaws.com/quickcast/%s/%s/quickcast.%s';
+					var fileCheck = '/%s/%s/quickcast.%s';
 
-						var amazonDetails = utilities.getAmazonDetails();
+					var amazonDetails = utilities.getAmazonDetails();
 
-						var s3 = require('aws2js').load('s3', amazonDetails.accessKeyId, amazonDetails.secretAccessKey)
+					var s3 = require('aws2js').load('s3', amazonDetails.accessKeyId, amazonDetails.secretAccessKey)
 
-						s3.setBucket(amazonDetails.destinationBucket);
+					s3.setBucket(amazonDetails.destinationBucket);
 
-						s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err, s3res) {
+					s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err4, s3res) {
 
-							var processed = null;
+						var processed = null;
 
-							if (err && err.code === 404){
-								processed = "processing";
-								//if (duration > 2)
-								//	processed = "failed";
-							}
-							else if (err && err.statusCode != 200)
-								processed = "failed";
+						if (err4 && err4.code === 404){
+							processed = "processing";
+							//if (duration > 2)
+							//	processed = "failed";
+						}
+						else if (err4 && err4.statusCode != 200)
+							processed = "failed";
 
-						    res.render('video', {
-								mp4: util.format(str, data.ownerid, data.castid, 'mp4'),
-								webm: util.format(str, data.ownerid, data.castid, 'webm'),
-								body: content,
-								views: data.views + r,
-								title: data.name,
-								username: data.username,
-								when: a.from(b),
-								processed: processed,
-								id: data.castid,
-								pageTitle: data.name,
-								video_width: data.width,
-								video_height: data.height,
-								uniqueid: video_entry.toLowerCase(),
-								tags: tags
-							});
-
+					    res.render('video', {
+							mp4: util.format(str, data.ownerid, data.castid, 'mp4'),
+							webm: util.format(str, data.ownerid, data.castid, 'webm'),
+							body: content,
+							views: data.views + r,
+							title: data.name,
+							username: data.username,
+							when: a.from(b),
+							processed: processed,
+							id: data.castid,
+							pageTitle: data.name,
+							video_width: data.width,
+							video_height: data.height,
+							uniqueid: video_entry.toLowerCase(),
+							tags: tags
 						});
-
-						
 					});
-
 				});
 			});
-		}else{
-			res.render('404', 404);
-		}
+		});
 	});
 };
 
@@ -113,49 +118,54 @@ exports.embed = function(req, res) {
     client.connect();
 
     client.query("SELECT casts.*, users.username FROM casts INNER JOIN users ON (casts.ownerid = users.userid) WHERE lower(casts.uniqueid) = $1 AND casts.published = true", [video_entry.toLowerCase()], function(err1, result1){
-		if (!err1 && result1 != undefined && result1.rowCount > 0){
-			var data = result1.rows[0];
+		if (err1){
+			res.render('500', 500);
+    		return;
+		}
 
-			var a = moment(data.created);
-			var b = moment(new Date());
+		if (result1 === undefined || !result1.rows){
+			res.render('404', 404);
+			return;
+		}
+			
+		var data = result1.rows[0];
 
-			var duration = moment(data.created).hours();
+		var a = moment(data.created);
+		var b = moment(new Date());
 
-			var str = 'https://s3.amazonaws.com/quickcast/%s/%s/quickcast.%s';
-			var fileCheck = '/%s/%s/quickcast.%s';
+		var duration = moment(data.created).hours();
 
-			var amazonDetails = utilities.getAmazonDetails();
+		var str = 'https://s3.amazonaws.com/quickcast/%s/%s/quickcast.%s';
+		var fileCheck = '/%s/%s/quickcast.%s';
 
-			var s3 = require('aws2js').load('s3', amazonDetails.accessKeyId, amazonDetails.secretAccessKey)
+		var amazonDetails = utilities.getAmazonDetails();
 
-			s3.setBucket(amazonDetails.destinationBucket);
+		var s3 = require('aws2js').load('s3', amazonDetails.accessKeyId, amazonDetails.secretAccessKey)
 
-			s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err, s3res) {
+		s3.setBucket(amazonDetails.destinationBucket);
 
-				var processed = null;
+		s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err3, s3res) {
 
-				if (err && err.code === 404){
-					processed = "processing";
-					if (duration > 2)
-						processed = "failed";
-				}
-				else if (err && err.statusCode != 200)
+			var processed = null;
+
+			if (err3 && err3.code === 404){
+				processed = "processing";
+				if (duration > 2)
 					processed = "failed";
+			}
+			else if (err3 && err3.statusCode != 200)
+				processed = "failed";
 
-			    res.render('embed', {
-					mp4: util.format(str, data.ownerid, data.castid, 'mp4'),
-					webm: util.format(str, data.ownerid, data.castid, 'webm'),
-					processed: processed,
-					id: data.castid,
-					video_width: data.width,
-					video_height: data.height,
-					uniqueid: video_entry.toLowerCase()
-				});
-
+		    res.render('embed', {
+				mp4: util.format(str, data.ownerid, data.castid, 'mp4'),
+				webm: util.format(str, data.ownerid, data.castid, 'webm'),
+				processed: processed,
+				id: data.castid,
+				video_width: data.width,
+				video_height: data.height,
+				uniqueid: video_entry.toLowerCase()
 			});
 
-		}else{
-			res.render('404', 404);
-		}
+		});
 	});
 };
