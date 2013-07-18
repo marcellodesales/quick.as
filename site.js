e701e86751b0ea4859f73025ba100113ffd6f175
Neil Kinnish <neilkinnish@Neils-MacBook-Pro.local>
2013-07-18 20:41:53 +0100

    updated

--- site.js
+++ site.js
@@ -31,11 +31,11 @@ exports.video = function(req, res) {
 
     client.connect();
 
-    client.query("SELECT casts.*, users.username, tags.* FROM casts INNER JOIN users ON (casts.ownerid = users.userid) INNER JOIN WHERE lower(casts.uniqueid) = $1 AND casts.published = true", [video_entry.toLowerCase()], function(err1, result1){
+    client.query("SELECT casts.*, users.username, tags.* FROM casts INNER JOIN users ON (casts.ownerid = users.userid) WHERE lower(casts.uniqueid) = $1 AND casts.published = true", [video_entry.toLowerCase()], function(err1, result1){
     	client.end();
 
     	if (err1) {
-    		res.render('500', 500);
+    		res.render('500', { error: err1 }, 500);
     		return;
     	}
 
@@ -46,19 +46,19 @@ exports.video = function(req, res) {
 		
 		var data = result1.rows[0];
 
-			client.query("SELECT tags.name FROM casts_tags INNER JOIN tags ON (casts_tags.tagid = tags.tagid) WHERE casts_tags.castid = $1", [data.castid], function(err2, result2){
+		client.query("SELECT tags.name FROM casts_tags INNER JOIN tags ON (casts_tags.tagid = tags.tagid) WHERE casts_tags.castid = $1", [data.castid], function(err2, result2){
 				
 			client.end();
 
 			var tags = null;
 
-			if (!err2 && result2 != undefined && result2.rows){
-				tags = result2.rows;
-			}
+			//if (!err2 && result2 != undefined && result2.rows){
+			//	tags = result2.rows;
+			//}
 
-			utilities.logViews(video_entry, req, function(err, r) {
-				marked(data.description, markedOpts, function (err3, content) {
-					if (err3) {
+			utilities.logViews(video_entry, req, function(err3, r) {
+				marked(data.description, markedOpts, function (err4, content) {
+					if (err4) {
 						content = "Error converting Markdown!";
 					}
 
@@ -76,16 +76,16 @@ exports.video = function(req, res) {
 
 					s3.setBucket(amazonDetails.destinationBucket);
 
-					s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err4, s3res) {
+					s3.head(util.format(fileCheck, data.ownerid, data.castid, 'webm'), function (err5, s3res) {
 
 						var processed = null;
 
-						if (err4 && err4.code === 404){
+						if (err5 && err5.code === 404){
 							processed = "processing";
 							//if (duration > 2)
 							//	processed = "failed";
 						}
-						else if (err4 && err4.statusCode != 200)
+						else if (err5 && err5.statusCode != 200)
 							processed = "failed";
 
 					    res.render('video', {
@@ -119,7 +119,7 @@ exports.embed = function(req, res) {
 
     client.query("SELECT casts.*, users.username FROM casts INNER JOIN users ON (casts.ownerid = users.userid) WHERE lower(casts.uniqueid) = $1 AND casts.published = true", [video_entry.toLowerCase()], function(err1, result1){
 		if (err1){
-			res.render('500', 500);
+			res.render('500', { error: err1 }, 500);
     		return;
 		}
 
