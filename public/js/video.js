9c49adc3227d9c39fd204f89f879b744e0484169
Neil Kinnish <neilkinnish@Neils-MacBook-Pro.local>
2013-07-29 23:40:58 +0100

    updated:

--- public/js/video.js
+++ public/js/video.js
@@ -6,10 +6,11 @@ $(function() {
 			$("a.embed span").hide();
 			var video_width = $(this).attr("data-width");
 			var video_height = $(this).attr("data-height");
+			var ratio = parseFloat(video_width/video_height);
 			if (video_width > 300 && video_height > 300){
 				$(this).append("<textarea><iframe name='quickcast' src='http://" + window.location.host + "/embed" + window.location.pathname + "' scrolling='no' frameborder='0' width='100%' allowfullscreen></iframe><script>!function(){function n(){var n=document.getElementsByName('quickcast')
 for(var e in n){var t=n[e].offsetWidth
-n[e].height=t/1.6+'px'}}n(),window.onresize=n,window.addEventListener('message',function(n){window.location.href=n.data},!1)}()
+n[e].height=t/" + ratio.toFixed(2) + "'px'}}n(),window.onresize=n,window.addEventListener('message',function(n){window.location.href=n.data},!1)}()
 </script></textarea>");
 			}else{
 				$(this).append("<textarea><iframe name='quickcast' src='http://" + window.location.host + "/embed" + window.location.pathname + "' scrolling='no' frameborder='0' width='" + video_width + "' height='" + video_height + "' allowfullscreen></iframe></textarea>");
@@ -20,6 +21,7 @@ n[e].height=t/1.6+'px'}}n(),window.onresize=n,window.addEventListener('message',
 });
 
 (function($) {
+	// Player initially based on http://www.inserthtml.com/2013/03/custom-html5-video-player/
 	$.fn.quickCastPlayer = function(options) {
 
 		// Add controls to mobile version and then return before quickcast player added
