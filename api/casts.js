769a19df95d9f5fa5c525599d485724e6f5f6024
Neil Kinnish <neilkinnish@Neils-MacBook-Pro-2.local>
2013-08-13 10:32:41 +0100

    updates to api

--- api/casts.js
+++ api/casts.js
@@ -13,66 +13,21 @@ AWS.config.update({accessKeyId: amazonDetails.accessKeyId, secretAccessKey: amaz
 // Publish, called as soon as the user confirms they wish to publish their quickcast
 // Expects a valid user token and responds with a castid and a temporary 
 // amazon s3 token allowing the app to begin uploading the raw mp4
-exports.publish = function() {
-	return function(req, res) {
-		// Validate user token - this could be middleware
-		token.validateToken(req, function(err, result){
+exports.publish = function(req, res) {
+	// Validate user token - this could be middleware
+	token.validateToken(req, function(err, result){
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
+			return;
+		}
+
+		var sts = new AWS.STS();
+		var params = { 'Name' : 'Temporary', 'Policy' : '{"Statement":[{"Effect":"Allow","Action":["s3:GetObject","s3:PutObject","s3:PutObjectAcl","s3:ListMultipartUploadParts","s3:AbortMultipartUpload","s3:GetObjectTorrent"],"Resource":"arn:aws:s3:::quickcast/' + result.user.userid + '/*"}]}', 'DurationSeconds' : 1200 };
+
+		// Get a temp amazon s3 token
+		sts.client.getFederationToken(params, function(err, data){
 			if (err) {
-				res.json({ status: 401, message: err }, 401);
-				return;
-			}
-
-			var sts = new AWS.STS();
-			var params = { 'Name' : 'Temporary', 'Policy' : '{"Statement":[{"Effect":"Allow","Action":["s3:GetObject","s3:PutObject","s3:PutObjectAcl","s3:ListMultipartUploadParts","s3:AbortMultipartUpload","s3:GetObjectTorrent"],"Resource":"arn:aws:s3:::quickcast/' + result.user.userid + '/*"}]}', 'DurationSeconds' : 1200 };
-
-			// Get a temp amazon s3 token
-			sts.client.getFederationToken(params, function(err, data){
-				if (err) {
-					res.json({ status: 500, message: err, amazon: amazonDetails }, 500);
-					return;
-				}
-
-				var pgClient = new pg.Client(config.postgres.connection);
-				pgClient.connect();
-
-				var response = {};
-
-				response["federationToken"] = data;
-				response["bucket-1"] = amazonDetails.sourceBucket;
-				response["bucket-2"] = amazonDetails.destinationBucket;
-				response["user"] = result.user;
-
-				var cleanTags = [];
-				var tags = req.body.tags.split(',');
-
-				for(var tag in tags){
-					var t = tags[tag].replace(/^\s*|\s*$/g, '');
-					if (t.length > 0)
-						cleanTags.push(t);
-				}
-
-				// Add any details to the cast table and get an id, this postgres function
-				// handles normalisation of tags, etc
-				pgClient.query("SELECT AddCast($1,$2,$3,$4,$5,$6,$7);", [result.user.userid, new Date(), req.body.description, req.body.name, req.body.intro, req.body.outro, cleanTags.join(",")])
-					.on('row', function(r){
-						response["cast"] = r;
-					})
-					.on('end', function(r){
-						pgClient.end();
-						res.json(response);
-					});			
-			});
-		});
-	};
-};
-
-// publish update. called by the app when the user submits the meta info for the quickcast
-exports.publishUpdate = function() {
-	return function(req, res) {
-		// Validate user token - this could be middleware
-		token.validateToken(req, function(err, result){
-			if (err) {
-				res.json({ status: 401, message: err }, 401);
+				res.json({ status: 500, message: err, amazon: amazonDetails }, 500);
 				return;
 			}
 
@@ -80,6 +35,11 @@ exports.publishUpdate = function() {
 			pgClient.connect();
 
 			var response = {};
+
+			response["federationToken"] = data;
+			response["bucket-1"] = amazonDetails.sourceBucket;
+			response["bucket-2"] = amazonDetails.destinationBucket;
+			response["user"] = result.user;
 
 			var cleanTags = [];
 			var tags = req.body.tags.split(',');
@@ -90,120 +50,152 @@ exports.publishUpdate = function() {
 					cleanTags.push(t);
 			}
 
-			// as per publish, updatecast handles tags and all meta data normalisation
-			pgClient.query("SELECT UpdateCast($1,$2,$3,$4,$5,$6);", [req.body.description, req.body.name, req.body.intro, req.body.outro, req.body.castid, cleanTags.join(",")])
+			// Add any details to the cast table and get an id, this postgres function
+			// handles normalisation of tags, etc
+			pgClient.query("SELECT AddCast($1,$2,$3,$4,$5,$6,$7);", [result.user.userid, new Date(), req.body.description, req.body.name, req.body.intro, req.body.outro, cleanTags.join(",")])
 				.on('row', function(r){
 					response["cast"] = r;
 				})
 				.on('end', function(r){
 					pgClient.end();
 					res.json(response);
-				});
+				});			
 		});
-	};
+	});
+};
+
+// publish update. called by the app when the user submits the meta info for the quickcast
+exports.publishUpdate = function(req, res) {
+	// Validate user token - this could be middleware
+	token.validateToken(req, function(err, result){
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
+			return;
+		}
+
+		var pgClient = new pg.Client(config.postgres.connection);
+		pgClient.connect();
+
+		var response = {};
+
+		var cleanTags = [];
+		var tags = req.body.tags.split(',');
+
+		for(var tag in tags){
+			var t = tags[tag].replace(/^\s*|\s*$/g, '');
+			if (t.length > 0)
+				cleanTags.push(t);
+		}
+
+		// as per publish, updatecast handles tags and all meta data normalisation
+		pgClient.query("SELECT UpdateCast($1,$2,$3,$4,$5,$6);", [req.body.description, req.body.name, req.body.intro, req.body.outro, req.body.castid, cleanTags.join(",")])
+			.on('row', function(r){
+				response["cast"] = r;
+			})
+			.on('end', function(r){
+				pgClient.end();
+				res.json(response);
+			});
+	});
 };
 
 // encode request. called once the raw mp4 has been uploaded and fires amazon encoding
-exports.encodeRequest = function() {
-	return function(req, res) {
-		if (req.headers.castid === undefined) {
-			res.json({ status: 400, message: "Invalid castid" }, 400); 
+exports.encodeRequest = function(req, res) {
+	if (req.headers.castid === undefined) {
+		res.json({ status: 400, message: "Invalid castid" }, 400); 
+		return;
+	}
+
+	// Validate user token - this could be middleware
+	token.validateToken(req, function(err, result){
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
 			return;
 		}
 
-		// Validate user token - this could be middleware
-		token.validateToken(req, function(err, result){
-			if (err) {
-				res.json({ status: 401, message: err }, 401);
+		// fire a message to amazon and start transcoding the video into mp4 and webm
+		var str = '%s/%s/quickcast.%s';
+		var strSmall = '%s/%s/quickcast-small.%s';
+
+		var et = new AWS.ElasticTranscoder();
+
+		var params = { 
+			'PipelineId': amazonDetails.pipelineId,
+			'Input': {
+				'Key': util.format(str, result.user.userid, req.headers.castid, 'mp4'),
+				'FrameRate': 'auto',
+				'Resolution': 'auto',
+				'AspectRatio': 'auto',
+				'Interlaced': 'auto',
+				'Container': 'auto'
+			},
+			'Outputs': [
+				{
+					'Key': util.format(strSmall, result.user.userid, req.headers.castid, 'mp4'),
+					'PresetId': amazonDetails.mp4small,
+					'ThumbnailPattern': "",
+					'Rotate': '0'
+				},
+				{
+					'Key': util.format(str, result.user.userid, req.headers.castid, 'webm'),
+					'PresetId': amazonDetails.webM,
+					'ThumbnailPattern': "",
+					'Rotate': '0'
+				}
+			]
+		};
+
+		// transcode
+		et.createJob(params, function(err1, data1){
+			if (err1){
+				res.json({ status: 500, message: err1 }, 500);
 				return;
 			}
 
-			// fire a message to amazon and start transcoding the video into mp4 and webm
-			var str = '%s/%s/quickcast.%s';
-			var strSmall = '%s/%s/quickcast-small.%s';
-
-			var et = new AWS.ElasticTranscoder();
-
-			var params = { 
-				'PipelineId': amazonDetails.pipelineId,
-				'Input': {
-					'Key': util.format(str, result.user.userid, req.headers.castid, 'mp4'),
-					'FrameRate': 'auto',
-					'Resolution': 'auto',
-					'AspectRatio': 'auto',
-					'Interlaced': 'auto',
-					'Container': 'auto'
-				},
-				'Outputs': [
-					{
-						'Key': util.format(strSmall, result.user.userid, req.headers.castid, 'mp4'),
-						'PresetId': amazonDetails.mp4small,
-						'ThumbnailPattern': "",
-						'Rotate': '0'
-					},
-					{
-						'Key': util.format(str, result.user.userid, req.headers.castid, 'webm'),
-						'PresetId': amazonDetails.webM,
-						'ThumbnailPattern': "",
-						'Rotate': '0'
-					}
-				]
-			};
-
-			// transcode
-			et.createJob(params, function(err1, data1){
-				if (err1){
-					res.json({ status: 500, message: err1 }, 500);
-					return;
-				}
-
-				res.json({ status: 200, message: "Encoding requested" }, 200);
-			});
+			res.json({ status: 200, message: "Encoding requested" }, 200);
 		});
-	};
+	});
 };
 
 // publish complete. sets the quickcast to published
-exports.publishComplete = function() {
-	return function(req, res) {
-		if (req.headers.castid === undefined) {
-			res.json({ status: 400, message: "Invalid castid" }, 400); 
+exports.publishComplete = function(req, res) {
+	if (req.headers.castid === undefined) {
+		res.json({ status: 400, message: "Invalid castid" }, 400); 
+		return;
+	}
+
+	// Validate user token - this could be middleware
+	token.validateToken(req, function(err, result){
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
 			return;
 		}
 
-		// Validate user token - this could be middleware
-		token.validateToken(req, function(err, result){
-			if (err) {
-				res.json({ status: 401, message: err }, 401);
-				return;
-			}
+		var pgClient = new pg.Client(config.postgres.connection);
+		pgClient.connect();
 
-			var pgClient = new pg.Client(config.postgres.connection);
-			pgClient.connect();
+		// get a hash for our unique video url
+		var hashids = new Hashids("quickyo"),
+			hash = hashids.encrypt(parseInt(result.user.userid), parseInt(req.headers.castid));
 
-			// get a hash for our unique video url
-			var hashids = new Hashids("quickyo"),
-				hash = hashids.encrypt(parseInt(result.user.userid), parseInt(req.headers.castid));
+		// update the database with the final details and mark the quickcast as published
+		pgClient.query("UPDATE casts SET published = true, size = $1, length = $2, width = $3, height = $4, uniqueid = $5 WHERE castid = $6", [req.headers.size, req.headers.length, parseInt(req.headers.width), parseInt(req.headers.height), hash, req.headers.castid])
+			.on('end', function(r) {
+				pgClient.end();
 
-			// update the database with the final details and mark the quickcast as published
-			pgClient.query("UPDATE casts SET published = true, size = $1, length = $2, width = $3, height = $4, uniqueid = $5 WHERE castid = $6", [req.headers.size, req.headers.length, parseInt(req.headers.width), parseInt(req.headers.height), hash, req.headers.castid])
-				.on('end', function(r) {
-					pgClient.end();
+				// send a postmark confirmation mail
+				var postmark = require("postmark")(utilities.getPostmark().apiKey);
 
-					// send a postmark confirmation mail
-					var postmark = require("postmark")(utilities.getPostmark().apiKey);
-
-					postmark.send({
-						"From": utilities.getPostmark().from, 
-						"To": result.user.email, 
-						"Subject": "Published QuickCast", 
-						"TextBody": "Hi " + result.user.firstname + ",\n\nYour QuickCast has been successfully published!\n\nIt can take a few seconds to encode your QuickCast once it has been uploaded, but once ready you can view online at the following URL: http://quick.as/" + hash + "\n\nThanks for using QuickCast\n\nhttp://quickcast.io\nhttp://twitter.com/quickcast"
-					});
-
-					res.json({ status: 200, message: "Successfully published", url: "http://quick.as/" + hash }, 200);
+				postmark.send({
+					"From": utilities.getPostmark().from, 
+					"To": result.user.email, 
+					"Subject": "Published QuickCast", 
+					"TextBody": "Hi " + result.user.firstname + ",\n\nYour QuickCast has been successfully published!\n\nIt can take a few seconds to encode your QuickCast once it has been uploaded, but once ready you can view online at the following URL: http://quick.as/" + hash + "\n\nThanks for using QuickCast\n\nhttp://quickcast.io\nhttp://twitter.com/quickcast"
 				});
-		});
-	};
+
+				res.json({ status: 200, message: "Successfully published", url: "http://quick.as/" + hash }, 200);
+			});
+	});
 };
 
 // Casts API index page
--- api/users.js
+++ api/users.js
@@ -6,182 +6,174 @@ var jwt = require('jwt-simple'),
     pg = require('pg');
 
 // handles the user signup
-exports.signup = function() {
-	return function(req, res) {
-		var username = req.headers.username,
-			password = req.headers.password,
-			email = req.headers.email,
-			firstname = req.headers.firstname,
-			lastname = req.headers.lastname,
-			mailinglist = req.headers.mailinglist === undefined ? false : req.headers.mailinglist,
-			errors = [];
+exports.signup = function(req, res) {
+	var username = req.headers.username,
+		password = req.headers.password,
+		email = req.headers.email,
+		firstname = req.headers.firstname,
+		lastname = req.headers.lastname,
+		mailinglist = req.headers.mailinglist === undefined ? false : req.headers.mailinglist,
+		errors = [];
 
-		// really basic validation - could be better
-		function validate(){
-			if (!utilities.validateField(firstname))
-				errors.push( { name: "firstname", message: "Firstname is required" } );
-			if (!utilities.validateField(lastname))
-				errors.push( { name: "lastname", message: "Lastname is required" } );
-			if (!utilities.validateField(username))
-				errors.push( { name: "username", message: "Username is required" } );
-			if (!utilities.validateEmail(email))
-				errors.push( { name: "email", message: "Email is required & must be valid" } );
-			if (!utilities.validateField(password))
-				errors.push( { name: "password", message: "Password is required" } );
+	// really basic validation - could be better
+	function validate(){
+		if (!utilities.validateField(firstname))
+			errors.push( { name: "firstname", message: "Firstname is required" } );
+		if (!utilities.validateField(lastname))
+			errors.push( { name: "lastname", message: "Lastname is required" } );
+		if (!utilities.validateField(username))
+			errors.push( { name: "username", message: "Username is required" } );
+		if (!utilities.validateEmail(email))
+			errors.push( { name: "email", message: "Email is required & must be valid" } );
+		if (!utilities.validateField(password))
+			errors.push( { name: "password", message: "Password is required" } );
 
-			if (errors.length > 0)
-				throw new Error("Invalid data sent");
+		if (errors.length > 0)
+			throw new Error("Invalid data sent");
 
-			return;
-		}
+		return;
+	}
 
-		var pgClient = new pg.Client(config.postgres.connection);
-		pgClient.connect();
+	var pgClient = new pg.Client(config.postgres.connection);
+	pgClient.connect();
 
-		// validate the email doesn't already exist within postgres
-		function emailExists(){
-			var deferred = Q.defer();
-			pgClient.query("SELECT email FROM users WHERE email = $1", [email], function(err, result){
-				if (result.rowCount > 0){
-					errors.push( { name: "email", message: "Email already in use" } );
-					deferred.reject(new Error("Email already in use"));
-				}else{
-					deferred.resolve();
-				}
-			});
-			return deferred.promise;
-		}
+	// validate the email doesn't already exist within postgres
+	function emailExists(){
+		var deferred = Q.defer();
+		pgClient.query("SELECT email FROM users WHERE email = $1", [email], function(err, result){
+			if (result.rowCount > 0){
+				errors.push( { name: "email", message: "Email already in use" } );
+				deferred.reject(new Error("Email already in use"));
+			}else{
+				deferred.resolve();
+			}
+		});
+		return deferred.promise;
+	}
 
-		// validate the username doesn't already exist within postgres
-		function usernameExists(){
-			var deferred = Q.defer();
-			pgClient.query("SELECT username FROM users WHERE username = $1", [username], function(err, result){
-				if (result.rowCount > 0){
-					errors.push( { name: "username", message: "Username already in use" } );
-					deferred.reject(new Error("Username already in use"));
-				}else{
-					deferred.resolve();
-				}
-			});
-			return deferred.promise;
-		}
+	// validate the username doesn't already exist within postgres
+	function usernameExists(){
+		var deferred = Q.defer();
+		pgClient.query("SELECT username FROM users WHERE username = $1", [username], function(err, result){
+			if (result.rowCount > 0){
+				errors.push( { name: "username", message: "Username already in use" } );
+				deferred.reject(new Error("Username already in use"));
+			}else{
+				deferred.resolve();
+			}
+		});
+		return deferred.promise;
+	}
 
-		// rather than nest, we defer all of the calls before we insert the new user
-		Q.fcall(validate)
-			.then(emailExists)
-			.then(usernameExists)
-			.then(function(){
-				utilities.cryptPassword(password,function(err,pwd){
-					pgClient.query("INSERT INTO users(created,firstname,lastname,email,username,password,mailinglist) VALUES ($1,$2,$3,$4,$5,$6,$7)", [new Date(), firstname, lastname, email, username, pwd, mailinglist])
-						.on('end', function(){
-							pgClient.end();
-							var token = jwt.encode({ email: email }, utilities.getSecret());
+	// rather than nest, we defer all of the calls before we insert the new user
+	Q.fcall(validate)
+		.then(emailExists)
+		.then(usernameExists)
+		.then(function(){
+			utilities.cryptPassword(password,function(err,pwd){
+				pgClient.query("INSERT INTO users(created,firstname,lastname,email,username,password,mailinglist) VALUES ($1,$2,$3,$4,$5,$6,$7)", [new Date(), firstname, lastname, email, username, pwd, mailinglist])
+					.on('end', function(){
+						pgClient.end();
+						var token = jwt.encode({ email: email }, utilities.getSecret());
 
-							// send a postmark confirmation mail
-							var postmark = require("postmark")(utilities.getPostmark().apiKey);
+						// send a postmark confirmation mail
+						var postmark = require("postmark")(utilities.getPostmark().apiKey);
 
-							postmark.send({
-								"From": utilities.getPostmark().from, 
-								"To": email, 
-								"Subject": "Welcome to QuickCast", 
-								"TextBody": "Hi " + firstname + ",\n\nThis email is simply to comfirm that you have created an account and published your first QuickCast!\n\nThanks for using QuickCast\n\nhttp://quickcast.io\nhttp://twitter.com/quickcast"
-							});
-
-							res.json({ token: token });
+						postmark.send({
+							"From": utilities.getPostmark().from, 
+							"To": email, 
+							"Subject": "Welcome to QuickCast", 
+							"TextBody": "Hi " + firstname + ",\n\nThis email is simply to comfirm that you have created an account and published your first QuickCast!\n\nThanks for using QuickCast\n\nhttp://quickcast.io\nhttp://twitter.com/quickcast"
 						});
-				});
-			})
-			.fail(function(err){
-				pgClient.end();
-				res.json({ errors: errors }, 403); 
-			})
-			.done();
-	};
+
+						res.json({ token: token });
+					});
+			});
+		})
+		.fail(function(err){
+			pgClient.end();
+			res.json({ errors: errors }, 403); 
+		})
+		.done();
 };
 
 // handles the user signin
-exports.signin = function() {
-	return function(req, res) {
-		var username = req.headers.username,
-			password = req.headers.password,
-			error = { status: 401, message: "Authentication failed for supplied credentials" };
+exports.signin = function(req, res) {
+	var username = req.headers.username,
+		password = req.headers.password,
+		error = { status: 401, message: "Authentication failed for supplied credentials" };
 
-		if (username === undefined || password === undefined) {
+	if (username === undefined || password === undefined) {
+		res.json(error, 401);
+		return;
+	}
+
+	var pgClient = new pg.Client(config.postgres.connection);
+	pgClient.connect();
+
+	// query postgres
+	pgClient.query("SELECT * FROM users WHERE username = $1", [username], function(err, result){
+		pgClient.end();
+		if (err) {
 			res.json(error, 401);
 			return;
 		}
 
-		var pgClient = new pg.Client(config.postgres.connection);
-		pgClient.connect();
+		if (result.rowCount === 0)
+		  res.json(error, 401);
+		else{
+			var row = result.rows[0];
 
-		// query postgres
-		pgClient.query("SELECT * FROM users WHERE username = $1", [username], function(err, result){
-			pgClient.end();
-			if (err) {
-				res.json(error, 401);
-				return;
-			}
-
-			if (result.rowCount === 0)
-			  res.json(error, 401);
-			else{
-				var row = result.rows[0];
-
-				// compare the encrypted password
-				utilities.comparePassword(password, row.password, function(err, match){
-					if (match)
-					{
-						var token = utilities.encodeToken({ email: row.email });
-						res.json({ token: token, email: row.email });
-					}
-					else
-						res.json(error, 401);
-				});
-			}
-		});
-	};
+			// compare the encrypted password
+			utilities.comparePassword(password, row.password, function(err, match) {
+				if (match)
+				{
+					var token = utilities.encodeToken({ email: row.email });
+					res.json({ token: token, email: row.email });
+				}
+				else
+					res.json(error, 401);
+			});
+		}
+	});
 };
 
 // handles the token validation by the app
-exports.userByToken = function() {
-	return function(req, res) {
-		token.validateToken(req, function(err, result){
-			if (err) {
-				res.json({ status: 401, message: err }, 401);
-				return;
-			}
-			
-			res.json(result);
-		});
-	};
+exports.userByToken = function(req, res) {
+	token.validateToken(req, function(err, result) {
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
+			return;
+		}
+		
+		res.json(result);
+	});
 };
 
 // returns a list (top 10) of users casts
-exports.userCasts = function() {
-	return function(req, res) {
-		token.validateToken(req, function(err, result){
-			if (err) {
-				res.json({ status: 401, message: err }, 401);
+exports.userCasts = function(req, res) {
+	token.validateToken(req, function(err, result){
+		if (err) {
+			res.json({ status: 401, message: err }, 401);
+			return;
+		}
+
+		var pgClient = new pg.Client(config.postgres.connection);
+		pgClient.connect();
+		
+		pgClient.query("SELECT * FROM casts WHERE ownerid = $1 AND published = true ORDER BY created DESC LIMIT 10", [result.user.userid], function(e, casts){
+			pgClient.end();
+			if (e) {
+				res.json(e, 400);
 				return;
 			}
 
-			var pgClient = new pg.Client(config.postgres.connection);
-			pgClient.connect();
-			
-			pgClient.query("SELECT * FROM casts WHERE ownerid = $1 AND published = true ORDER BY created DESC LIMIT 10", [result.user.userid], function(e, casts){
-				pgClient.end();
-				if (e) {
-					res.json(e, 400);
-					return;
-				}
-
-				if (casts.rowCount === 0)
-					res.json({ casts: null, user: result.user }, 200)
-				else
-					res.json({ casts: casts, user: result.user }, 200);
-			});
+			if (casts.rowCount === 0)
+				res.json({ casts: null, user: result.user }, 200)
+			else
+				res.json({ casts: casts, user: result.user }, 200);
 		});
-	};
+	});
 };
 
 // Users API index page
